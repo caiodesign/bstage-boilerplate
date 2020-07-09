2ed9b3f8f838ae3e11f2562d8586b7fadebca562
Caio Oliveira <colive.dev@gmail.com>
2020-07-09 01:52:39 -0300

    - storybook setup - simple storie for Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'bstage',
+  description = 'TypeScript, NextJS, ReactJS e Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/icon-512.png"
       alt="Imagem de um átomo e bstage escrito ao lado."
     />
-    <S.Title>bstage</S.Title>
-    <S.Description>
-      TypeScript, NextJS, ReactJS e Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvendor de frente para uma tela com código."
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,15 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+}
+
+export const Basic = () => (
+  <Main
+    title={text('Title', 'Storybook')}
+    description={text('Description', 'is Awesome')}
+  />
+)
