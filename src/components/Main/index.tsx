ff8c4bd5a63b7e1364bab99be4c432ba9d6169c0
Caio Oliveira <colive.dev@gmail.com>
2020-07-09 00:47:31 -0300

    - jest-styled-components setup - add new style and content into Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>bstage</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/icon-512.png"
+      alt="Imagem de um átomo e bstage escrito ao lado."
+    />
+    <S.Title>bstage</S.Title>
+    <S.Description>
+      TypeScript, NextJS, ReactJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvendor de frente para uma tela com código."
+    />
+  </S.Wrapper>
 )
 
 export default Main
